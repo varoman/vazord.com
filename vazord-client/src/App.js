d9502c3ac32d6b5651debce8eaa9dd467953d02a
varo <varazdat.manukyan@click2sure.co.za>
2019-11-23 18:21:45 +0400

    test it

--- vazord-client/src/App.js
+++ vazord-client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Sidebar, Article, Admin, AppHeader, Welcome }  from './components';
 import './App.css';
 
-alert('worked')
+alert('worked, yeah');
 
 const { Content, Footer } = Layout;
 
