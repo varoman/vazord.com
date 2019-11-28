bd755cd81f04099cd5099bb99e8fc7866892a111
varo <varazdat.manukyan@click2sure.co.za>
2019-11-28 11:13:59 +0400

    add mailgun functionality

--- vazord-backend/src/modules/emails/index.js
+++ vazord-backend/src/modules/emails/index.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
-const { EMAIL_ACCOUNT, EMAIL_PASSWORD } = process.env;
+const mailgun = require('mailgun-js');
+const { EMAIL_ACCOUNT, EMAIL_PASSWORD, MG_DOMAIN, MG_API_KEY, NODE_ENV } = process.env;
 
 
 const transporter = nodemailer.createTransport({
@@ -10,8 +11,7 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-
-const sendMail = ({ to, subject, content }) => {
+const sendGMail = ({ to, subject, content }) => {
     transporter.sendMail({
         from: 'vazord@info.com',
         to,
@@ -19,6 +19,22 @@ const sendMail = ({ to, subject, content }) => {
         html: content
     });
 };
+
+const sendMgMail = ({ to, subject, content }) => {
+    const mg = mailgun({ apiKey: MG_API_KEY, MG_DOMAIN });
+    const data = {
+        from: '<vazord@mg.vazord.com>',
+        to,
+        subject,
+        html: content,
+    };
+    mg.messages().send(data, function (error, body) {
+        console.log(body);
+    });
+};
+
+
+const sendMail = NODE_ENV === 'heroku' ? sendMgMail : sendGMail;
 
 
 module.exports = sendMail;
