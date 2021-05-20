4163bf151977fd12bc73462cc7e59d5c7aeb098c
Varazdat Manukyan <varazdat.manukyan@click2sure.co.za>
2021-05-20 18:06:27 +0400

    add db config

--- vazord-backend/src/db/index.js
+++ vazord-backend/src/db/index.js
@@ -1,9 +1,18 @@
 const Sequelize = require('sequelize');
-const { NODE_ENV } = process.env;
-const { username, password, database, host, dialect } = require('./config/database')[NODE_ENV];
-const { DATABASE_URL } = process.env;
+// const { NODE_ENV } = process.env;
+// const { username, password, database, host, dialect } = require('./config/database')[NODE_ENV];
+// const { DATABASE_URL } = process.env;
 
-const options = DATABASE_URL ? [ DATABASE_URL ] : [ database, username, password, { host, dialect } ];
+const options = [
+    'd37bivntj1ldkh', 'ofsvsneglfuaep',
+    '4086875ca67668b8c5d5f9b8031209f29b8dd431cd9e7f2f54053c5d5c414ffd',
+    { host: 'ec2-184-73-176-11.compute-1.amazonaws.com',
+        dialect: 'postgres',
+        dialectOptions: {
+        ssl: {require:true}
+    }
+    },
+];
 const sequelize = new Sequelize(...options);
 
 sequelize
